a2e06c0a3e7e7606d902c406bb6b6186007735c8
Mohr-MedicalEthics <152247292+Mohr-MedicalEthics@users.noreply.github.com>
2023-12-04 18:45:20 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,13 @@
 var philosophers = {
     "Aristotle": [3, 3, 2, 4, 2, 3],
     "Friedrich Nietzsche": [2, 3, 3, 3, 4, 2],
-    "Imnmanuel Kant": [1, 1, 1, 5, 1, 1],
+    "Immanuel Kant": [1, 1, 1, 5, 1, 1],
     "John Stuart Mill": [5, 4, 4, 3, 2, 4],
     "John Rawls": [3, 3, 3, 3, 2, 4],
     "Philippa Foot": [4, 2, 2, 4, 3, 3],
     "Karl Marx": [2, 4, 3, 2, 1, 2],
     "Socrates": [3, 2, 2, 4, 3, 3],
     "John Locke": [4, 3, 3, 5, 1, 2],
-    //correct philosophers below to match according to the correct data
     "Bertrand Russell": [4, 3, 3, 3, 3, 3],
     "Thomas Hobbes": [1, 1, 1, 1, 1, 1],
     "G.E. Moore": [1, 2, 3, 4, 5, 6]
@@ -26,9 +25,8 @@ function calculateMatch(answers) {
         var score = 0;
 
         // Calculate the score for this philosopher
-        for (var i = 1; i <= 6; i++) {
-            var answer = answers['statement' + i];
-            score += Math.abs(answer - scores[i - 1]);
+        for (var i = 0; i < 6; i++) {
+            score += Math.abs(scores[i] - answers[i]);
         }
 
         // Check if this philosopher is the best match
@@ -48,21 +46,40 @@ function displayMatchedPhilosopher(matchedPhilosopher) {
     var philosopherImageElement = document.getElementById('philosopherImage');
 
     matchedPhilosopherElement.textContent = matchedPhilosopher;
-    philosopherImageElement.src = 'images/' + matchedPhilosopher.toLowerCase() + '.jpg'; // Replace 'images/' with the correct path to your philosopher images
+
+    // Image paths based on the philosopher's name
+    var imagePaths = {
+        "Aristotle": 'MedEthicsAristotle.jpeg',
+        "Friedrich Nietzsche": 'MedEthicsNietzsche.jpeg',
+        "Immanuel Kant": 'MedEthicsKant.jpeg',
+        "John Stuart Mill": 'MedEthicsJSMill.jpeg',
+        "John Rawls": 'MedEthicsRawls.jpeg',
+        "Philippa Foot": 'MedEthicsFoot.webp',
+        "Karl Marx": 'MedEthicsMarx.jpeg',
+        "Socrates": 'MedEthicsSocrates.png',
+        "John Locke": 'MedEthicsLocke.jpeg',
+        "Bertrand Russell": 'MedEthicsRussell.avi',
+        "Thomas Hobbes": 'MedEthicsHobbes.jpeg',
+        "G.E. Moore": 'MedEthicsMoore.jpeg'
+        // Add more philosophers and their corresponding images if needed
+    };
+
+    // Set the image source to the corresponding philosopher's image
+    philosopherImageElement.src = 'images/' + imagePaths[matchedPhilosopher] || 'images/placeholder.png';
+
     resultDiv.classList.remove('hidden');
 }
 
 // Function to calculate match and display result when the Submit button is clicked
 function calculateMatchAndDisplay() {
     var formData = new FormData(document.getElementById('surveyForm'));
-    var answers = {};
+    var answers = [];
 
     for (var pair of formData.entries()) {
-        var questionName = pair[0];
-        var answerValue = pair[1];
-        answers[questionName] = answerValue;
+        answers.push(pair[1]);
     }
 
     var matchedPhilosopher = calculateMatch(answers);
     displayMatchedPhilosopher(matchedPhilosopher);
 }
+
